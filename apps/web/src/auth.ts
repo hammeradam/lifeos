e9aac0c656763395de36c0c7b751eabafb1acd40
Adam Hammer <adam.hammer@fizz.hu>
2025-01-30 16:05:22 +0100

    debug VITE_API_BASE_URL

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 
+console.log(import.meta.env.VITE_API_BASE_URL);
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_BASE_URL, // the base url of your auth server
 });
